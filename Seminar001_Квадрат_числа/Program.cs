3e19626949da5f49338cd8bd58358c58e793a2d5
Dmitriy <semenovsdo@mail.ru>
2023-02-05 19:49:20 +0300

    Проверка, является ли одно число квадратом другого?

---
+++ Seminar001_Квадрат_числа/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите число № 1 ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число № 2 ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 == Math.Pow(number2, 2))
+{
+    Console.WriteLine("Число № 2 является квадратом числа 1");
+}
+else
+{
+    if (number2 == Math.Pow(number1, 2)) 
+    {
+        Console.WriteLine("Число № 1 является квадратом числа 2");
+    }
+    else
+    {
+        Console.WriteLine("Не являются");
+    }
+}
